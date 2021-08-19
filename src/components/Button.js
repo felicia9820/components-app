217b216d96a4853535e76cfc491316d4479bc80a
felicia9820 <feliciaroman2@gmail.com>
2021-08-19 14:30:50 +0200

    init commit

---
+++ src/components/Button.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
+
+class Button extends React.Component {
+    static contextType = LanguageContext;
+
+    render() {
+        
+        return (
+            <ColorContext.Consumer>
+                {(color) =>
+                    <button className={`ui button ${color}`}>
+                        <LanguageContext.Consumer>
+                            {({ language }) => language === 'english' ? 'Submit' : 'Vorleggen'}
+                        </LanguageContext.Consumer>
+                    </button>
+                }
+                
+            </ColorContext.Consumer>
+        );
+
+    };
+    
+};
+
+export default Button;
